b754aa712d789530ed8a089103ee24784f53c4e3
Keisuke Kan <9renpoto@gmail.com>
2017-08-19 11:52:05 +0900

    refacrtor: remove generate js files

--- __tests__/BrowserAction.js
+++
@@ -1,8 +0,0 @@
-jest.dontMock('./../lib/BrowserAction');
-var BrowserAction_1 = require('./../lib/BrowserAction');
-describe("BrowserAction", function () {
-    it("Create", function () {
-        var action = new BrowserAction_1.BrowserAction();
-        expect(action.onClicked.addListener).toBeDefined();
-    });
-});
--- index.js
+++
@@ -1,2 +0,0 @@
-var Chrome_1 = require('./lib/Chrome');
-exports.Chrome = Chrome_1.Chrome;
